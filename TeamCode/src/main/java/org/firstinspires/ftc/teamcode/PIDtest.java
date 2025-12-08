c2dac5b18ef56aab1486ff9c9946305981722a29
Glen <drumboog@gmail.com>
2025-12-08 18:57:53 -0500

    Add support for Feeder and Driver Motors to RampageRobot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDtest.java
@@ -21,16 +21,30 @@ public class PIDtest extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+            processInput(robot);
+
             robot.update();
 
-            TelemetryPacket packet = new TelemetryPacket();
-            packet.put("Left Desired Ticks/Sec", robot.getFlywheelLeft().getTargetPulsePerSecond());
-            packet.put("Left Measured Ticks/Sec", robot.getFlywheelLeft().getMeasuredPulsePerSecond());
-            packet.put("Left Power", robot.getFlywheelLeft().getPower());
-            packet.put("Right Desired Ticks/Sec", robot.getFlywheelRight().getTargetPulsePerSecond());
-            packet.put("Right Measured Ticks/Sec", robot.getFlywheelRight().getMeasuredPulsePerSecond());
-            packet.put("Right Power", robot.getFlywheelRight().getPower());
-            FtcDashboard.getInstance().sendTelemetryPacket(packet);
+            updateTelemetry(robot);
         }
+    }
+
+    private void processInput(RampageRobot robot) {
+        if (gamepad1.aWasPressed()) {
+            robot.toggleFeeder();
+        }
+//        robot.setDriveMotorPower(0.1, 0.1, 0.1, 0.1);
+    }
+
+    private void updateTelemetry(RampageRobot robot) {
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.put("Left Flywheel Desired Ticks/Sec", robot.getFlywheelLeft().getTargetPulsePerSecond());
+        packet.put("Left Flywheel Measured Ticks/Sec", robot.getFlywheelLeft().getMeasuredPulsePerSecond());
+        packet.put("Left Flywheel Power", robot.getFlywheelLeft().getPower());
+        packet.put("Right Flywheel Desired Ticks/Sec", robot.getFlywheelRight().getTargetPulsePerSecond());
+        packet.put("Right Flywheel Measured Ticks/Sec", robot.getFlywheelRight().getMeasuredPulsePerSecond());
+        packet.put("Right Flywheel Power", robot.getFlywheelRight().getPower());
+        packet.put("Is Feeder Closed", robot.isFeederClosed());
+        FtcDashboard.getInstance().sendTelemetryPacket(packet);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RampageRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RampageRobot.java
@@ -4,18 +4,64 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.robot.motors.FlywheelMotorController;
 
 public class RampageRobot {
+    private static final double FEEDER_CLOSED_POSITION = 0.25;
+    private static final double FEEDER_OPEN_POSITION = 0.82;
+
     private final OpMode opMode;
+
+    private final DcMotor frontLeftMotor;
+    private final DcMotor frontRightMotor;
+    private final DcMotor backLeftMotor;
+    private final DcMotor backRightMotor;
     private final FlywheelMotorController flywheelLeft;
     private final FlywheelMotorController flywheelRight;
 
+    private final Servo feeder;
+    private boolean isFeederClosed = false;
+
     public RampageRobot(OpMode opMode) {
         this.opMode = opMode;
+        this.frontLeftMotor = getDriveMotor("FrontLeft", DcMotorSimple.Direction.REVERSE);
+        this.frontRightMotor = getDriveMotor("FrontRight", DcMotorSimple.Direction.FORWARD);
+        this.backLeftMotor = getDriveMotor("BackLeft", DcMotorSimple.Direction.REVERSE);
+        this.backRightMotor = getDriveMotor("BackRight", DcMotorSimple.Direction.FORWARD);
         this.flywheelLeft = createFlywheelMotorController("FlywheelLeft", DcMotorSimple.Direction.REVERSE);
         this.flywheelRight = createFlywheelMotorController("FlywheelRight", DcMotorSimple.Direction.FORWARD);
+        this.feeder = opMode.hardwareMap.get(Servo.class, "Feeder");
+    }
+
+    public void setDriveMotorPower(double frontLeft, double frontRight, double backLeft, double backRight) {
+        frontLeftMotor.setPower(frontLeft);
+        frontRightMotor.setPower(frontRight);
+        backLeftMotor.setPower(backLeft);
+        backRightMotor.setPower(backRight);
+    }
+
+    public boolean isFeederClosed() {
+        return this.isFeederClosed;
+    }
+
+    public void closeFeeder() {
+        isFeederClosed = true;
+        feeder.setPosition(FEEDER_CLOSED_POSITION);
+    }
+
+    public void openFeeder() {
+        isFeederClosed = false;
+        feeder.setPosition(FEEDER_OPEN_POSITION);
+    }
+
+    public void toggleFeeder() {
+        if (isFeederClosed) {
+            openFeeder();
+        } else {
+            closeFeeder();
+        }
     }
 
     public FlywheelMotorController getFlywheelLeft() {
@@ -27,8 +73,15 @@ public class RampageRobot {
     }
 
     public void update() {
-        this.flywheelLeft.update();
-        this.flywheelRight.update();
+        flywheelLeft.update();
+        flywheelRight.update();
+    }
+
+    private DcMotor getDriveMotor(String deviceName, DcMotor.Direction direction) {
+        DcMotor motor = opMode.hardwareMap.get(DcMotor.class, deviceName);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setDirection(direction);
+        return motor;
     }
 
     private FlywheelMotorController createFlywheelMotorController(String deviceName, DcMotor.Direction direction) {
